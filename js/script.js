9d607f0dce2060fdecd9c6d48e3cd76b823d7ba0
ramawow <rmt0276@gmail.com>
2024-01-19 16:02:43 +0700

    add animation and fix responsive

--- js/script.js
+++ js/script.js
@@ -1,63 +1,73 @@
-$('.page-scroll').on('click', function(e){
+$('.page-scroll').on('click', function (e) {
 
-    //ambil isi href
-    var tujuan = $(this).attr('href');
-    //tangkap elemen ybs
-    var elemenTujuan = $(tujuan);
+	//ambil isi href
+	var tujuan = $(this).attr('href');
+	//tangkap elemen ybs
+	var elemenTujuan = $(tujuan);
 
-    //pindahkan scroll
-    $('html,body').animate({
-     scrollTop: elemenTujuan.offset().top -50   
-    }, 2000, 'easeInOutExpo');
+	//pindahkan scroll
+	$('html,body').animate({
+		scrollTop: elemenTujuan.offset().top - 50
+	}, 2000, 'easeInOutExpo');
 
-    e.preventDefault();
-    
+	e.preventDefault();
+
 });
 
+// Automatic text
+document.addEventListener('DOMContentLoaded', function () {
+	var options = {
+		strings: ["Programmer", "Web Developer", "Designer", "Freelancer"],
+		typeSpeed: 100,
+		backSpeed: 60,
+		loop: true
+	};
+	var typed = new Typed(".typing", options);
+});
 
 
 // Parallax
 // About
-$(window).on('load', function(){
+$(window).on('load', function () {
 	$('.pKiri').addClass('pMuncul');
 	$('.pKanan').addClass('pMuncul');
 })
 
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 	var wScroll = $(this).scrollTop();
 
 	// Jumbotron
 
 	$('.jumbotron img').css({
-		'transform' : 'translate(0px, '+ wScroll/4 +'%)'
+		'transform': 'translate(0px, ' + wScroll / 4 + '%)'
 	});
 
 	$('.jumbotron h1').css({
-		'transform' : 'translate(0px, '+ wScroll/2 +'%)'
+		'transform': 'translate(0px, ' + wScroll / 2 + '%)'
 	});
 
 	$('.jumbotron p').css({
-		'transform' : 'translate(0px, '+ wScroll/1.2 +'%)'
+		'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
 	});
 
 
 	// portofolio
-	if( wScroll > $('.portofolio').offset().top - 250) {
-		$('.portofolio .thumbnail').each(function(i){
-			setTimeout(function() {
+	if (wScroll > $('.portofolio').offset().top - 250) {
+		$('.portofolio .thumbnail').each(function (i) {
+			setTimeout(function () {
 				$('.portofolio .thumbnail').eq(i).addClass('muncul');
 				$('.portofolio h4').eq(i).addClass('muncul');
-			}, 400 * (i+1));
+			}, 400 * (i + 1));
 		});
 	}
 
-	if( wScroll > $('.sertificate').offset().top - 250) {
-		$('.sertificate .thumbnail').each(function(i){
-			setTimeout(function() {
+	if (wScroll > $('.sertificate').offset().top - 250) {
+		$('.sertificate .thumbnail').each(function (i) {
+			setTimeout(function () {
 				$('.sertificate .thumbnail').eq(i).addClass('muncul');
 				$('.sertificate h4').eq(i).addClass('muncul');
-			}, 400 * (i+1));
+			}, 400 * (i + 1));
 		});
 	}
 
