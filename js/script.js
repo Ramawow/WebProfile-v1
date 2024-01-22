82379167d7a84514ff6ff5a4ef6ecc8ea7f439e6
ramawow <rmt0276@gmail.com>
2024-01-22 20:14:29 +0700

    add portfolio, edit konten

--- js/script.js
+++ js/script.js
@@ -211,6 +211,15 @@ const portfolioLightbox = GLightbox({
 });
 
 /**
+   * Initiate portfolio details lightbox 
+   */
+const portfolioDetailsLightbox = GLightbox({
+	selector: '.portfolio-details-lightbox',
+	width: '90%',
+	height: '90vh'
+});
+
+/**
    * Portfolio details slider
    */
 new Swiper('.portfolio-details-slider', {
